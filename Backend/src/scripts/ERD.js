ca8adde36f12e0d541401c061bb2f5c7603bf0a6
YoussufAhmedEE <110684819+YoussufAhmedEE@users.noreply.github.com>
2025-05-07 13:40:04 +0300

    just rearrange the files

--- Backend/src/scripts/ERD.js
+++ Backend/src/scripts/ERD.js
@@ -2,5 +2,5 @@ const { db } = require('../models/associations');
 const erd = require('sequelize-erd');
 
 erd({ source: db }).then(svg => {
-  require('fs').writeFileSync('diagram.svg', svg);
+  require('fs').writeFileSync('ERD/diagram.svg', svg);
 });
